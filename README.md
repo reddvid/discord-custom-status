21351fd11a7ecf1debea23ef1b7f8724be0b7ddf
David Ballesteros <david_ballesteros@outlook.com>
2022-04-08 05:39:17 +0800

    Works on Windows

---
+++ README.md
@@ -0,0 +1,13 @@
+# discord-custom-status
+
+Set your Discord custom status automatically with your current active Window.
+
+## Inspired by Jyxme Covid-19 Discord Custom Status
+
+[Check Jyxme Repo Here](https://github.com/Jxyme/covid-19-discord-custom-status)
+
+#### WARNINGS also apply on this _**self-bot**_ (may result with Discord account BAN).
+
+### Tested on Windows 11
+
+#### Modified `active-window` dependecy to Get Product instead of ProcessName
---
+++ index.js
@@ -0,0 +1,52 @@
+const request = require("request");
+const config = require("./config.json");
+const monitor = require("./active-window");
+const URL = `https://discord.com/api/v10/users/@me/settings`;
+
+let previousApp;
+
+callback = function (window) {
+  try {
+    console.log(`App: ${window.app}`);
+    if (window.app != previousApp && window.app != "") {
+      previousApp = window.app;
+      updateStatus(window.app);
+    }
+  } catch (err) {
+    console.log(`Callback error: ${err}`);
+  }
+};
+
+//Get the current active window
+monitor.getActiveWindow(callback, -1, config.interval); // (callback, # of requests (-1 inf), interval in seconds)
+
+/* Update Status */
+function updateStatus(appName) {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        method: "PATCH",
+        uri: URL,
+        headers: {
+          Authorization: config.token,
+        },
+        json: {
+          custom_status: {
+            text: `${config.activityName} ${appName}`,
+            emoji_name: config.emojiName,
+          },
+        },
+      },
+      (err, res) => {
+        if (err) {
+          return reject(`Request error: ${res.statusCode}`);
+        }
+        if (res.statusCode !== 200) {
+          return reject(`Status code error: ${res.statusCode}`);
+        }
+        resolve(true);
+        console.log("Updated status");
+      }
+    );
+  });
+}
