7a714e9f0da3b57347b7c3384ccab59032ee8031
David Ballesteros <david_ballesteros@outlook.com>
2025-06-20 05:20:24 +0800

    fix status bug

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const request = require("request");
-const config = require("./config.json");
-const monitor = require("./active-window");
+const request = require('request');
+const config = require('./config.json');
+const monitor = require('./active-window');
 const URL = `https://discord.com/api/v10/users/@me/settings`;
 
 let previousApp;
+let previousTitle;
 let customStatus;
 let customEmoji;
 
@@ -15,11 +16,19 @@ switch (args) {
       try {
         console.log(`App: ${window.app}`);
         console.log(`Title: ${window.title}`);
-        if (window.app != previousApp) {
+        if (
+          (window.app != previousApp || window.title != previousTitle) &&
+          !window.app.includes('Microsoft')
+        ) {
           previousApp = window.app;
+          previousTitle = window.title;
           updateStatus(window.app);
-        } else if (window.app == "") {
-          previousApp = window.title;
+        } else if (
+          (window.app == '' || window.app.includes('Microsoft')) &&
+          (window.app != previousApp || window.title != previousTitle)
+        ) {
+          previousTitle = window.title;
+          previousApp = window.app;
           updateStatus(window.title);
         }
       } catch (err) {
@@ -29,10 +38,10 @@ switch (args) {
 
     monitor.getActiveWindow(callback, -1, config.interval);
     break;
-  case ("book", "read", "reading"):
-    customStatus = "Reading a physical book";
-    customEmoji = "📖";
-    updateStatus("custom");
+  case ('book', 'read', 'reading'):
+    customStatus = 'Reading a physical book';
+    customEmoji = '📖';
+    updateStatus('custom');
 }
 
 /* Update Status */
@@ -40,7 +49,7 @@ function updateStatus(appName) {
   return new Promise((resolve, reject) => {
     request(
       {
-        method: "PATCH",
+        method: 'PATCH',
         uri: URL,
         headers: {
           Authorization: config.token,
@@ -48,10 +57,10 @@ function updateStatus(appName) {
         json: {
           custom_status: {
             text:
-              appName === "custom"
+              appName === 'custom'
                 ? customStatus
                 : `${config.activityName} ${appName}`,
-            emoji_name: appName === "custom" ? customEmoji : config.emojiName,
+            emoji_name: appName === 'custom' ? customEmoji : config.emojiName,
           },
         },
       },
@@ -63,7 +72,7 @@ function updateStatus(appName) {
           return reject(`Status code error: ${res.statusCode}`);
         }
         resolve(true);
-        console.log("Updated status");
+        console.log('Updated status: ' + appName);
       }
     );
   }).catch((e) => console.log(e));
